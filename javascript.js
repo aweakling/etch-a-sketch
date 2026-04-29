8ef5e8a12c54989b6d2e1107f31a29a5449fba90
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-04-29 17:56:10 +0800

    Finish trails and add reset button

--- javascript.js
+++ javascript.js
@@ -1,31 +1,36 @@
 const container = document.querySelector("#container");
+const resetBtn = document.querySelector(".resetBtn");
 
 // create 16 squares
-for(let i = 1; i <= 16; i++){
-    const squares = document.createElement("div");
+function sketch(){    
+    for(let i = 1; i <= 16; i++){
+        const squares = document.createElement("div");
 
-    squares.classList.add("squares");
+        squares.classList.add("squares");
 
-    squares.style.boxSizing = "border-box";
+        squares.style.boxSizing = "border-box";
 
-    container.appendChild(squares);
+        container.appendChild(squares);
 
-    for(let j = 1; j <= 16; j++) {
-        const miniSquares = document.createElement("div");
+        for(let j = 1; j <= 16; j++) {
+            const miniSquares = document.createElement("div");
 
-        miniSquares.classList.add("miniSquares");
+            miniSquares.classList.add("miniSquares");
 
-        miniSquares.style.boxSizing = "border-box";
+            miniSquares.style.boxSizing = "border-box";
 
-        miniSquares.addEventListener("mouseenter", (e) => {
-            miniSquares.style.backgroundColor = "black";
-        });
+            miniSquares.addEventListener("mouseenter", (e) => {
+                miniSquares.style.backgroundColor = "black";
+            });
 
-        miniSquares.addEventListener("mouseleave", () => {
-            miniSquares.style.backgroundColor = "";
-        });
-
-        squares.appendChild(miniSquares);
+            squares.appendChild(miniSquares);
+        }
     }
 }
 
+resetBtn.addEventListener("click", () => {
+    container.innerHTML = "";
+    sketch();
+});
+
+sketch();
