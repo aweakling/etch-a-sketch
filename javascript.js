c721fdc72c8e508d263443bfcc5fb4c457c4ff47
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-04-29 18:57:13 +0800

    Rework sketch logic for scalability

--- javascript.js
+++ javascript.js
@@ -3,44 +3,69 @@ const resetBtn = document.querySelector(".resetBtn");
 const squareBtn = document.querySelector(".squareBtn");
 
 // create 16 squares
-function sketch(){    
-    squareBtn.addEventListener("click", () => {
-        let size = parseInt(prompt("Size of grid", 16));
+function sketch(size) {
+    container.innerHTML = "";
+    container.style.setProperty("--size", size);
 
-        if(size > 64) size = 64;
+    for(let i = 0; i < size * size; i++){
+        const squares = document.createElement("div");
+        squares.classList.add("squares");
 
-        container.innerHTML = "";
-
-        container.style.setProperty("--size", size);
-
-        for(let i = 0; i < size * size; i++){
-            const squares = document.createElement("div");
-
-            squares.classList.add("miniSquares");
-
-            squares.addEventListener("mouseenter", () => {
-                squares.style.backgroundColor = "black"
-            });
-
-            container.appendChild(squares);
-
-            // for(let j = 1; j <= size; j++) {
-            //     const miniSquares = document.createElement("div");
-
-            //     miniSquares.classList.add("miniSquares");
-
-            //     miniSquares.addEventListener("mouseenter", (e) => {
-            //         miniSquares.style.backgroundColor = "black";
-            //     });
-
-            //     squares.appendChild(miniSquares);
-            // }
-        }
-    });
+        squares.addEventListener("mouseenter", () => {
+            squares.style.backgroundColor = "black";
+        });
+        container.appendChild(squares);
+    }
 }
+
+sketch(16);
+
+squareBtn.addEventListener("click", () => {
+    let size = parseInt(prompt("Size of grid", 16));
+
+    if(size > 64) size = 64;
+
+    sketch(size);
+});
 
 resetBtn.addEventListener("click", () => {
     container.innerHTML = "";
+
+    sketch(16);
 });
 
-sketch();
+// function sketch(){    
+//     squareBtn.addEventListener("click", () => {
+//         let size = parseInt(prompt("Size of grid", 16));
+
+//         if(size > 64) size = 64;
+
+//         container.innerHTML = "";
+
+//         container.style.setProperty("--size", size);
+
+//         for(let i = 0; i < size * size; i++){
+//             const squares = document.createElement("div");
+
+//             squares.classList.add("miniSquares");
+
+//             squares.addEventListener("mouseenter", () => {
+//                 squares.style.backgroundColor = "black"
+//             });
+
+//             container.appendChild(squares);
+
+//             // for(let j = 1; j <= size; j++) {
+//             //     const miniSquares = document.createElement("div");
+
+//             //     miniSquares.classList.add("miniSquares");
+
+//             //     miniSquares.addEventListener("mouseenter", (e) => {
+//             //         miniSquares.style.backgroundColor = "black";
+//             //     });
+
+//             //     squares.appendChild(miniSquares);
+//             // }
+//         }
+//     });
+// }
